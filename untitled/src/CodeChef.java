2d79028e00fb596a3c5a40ed5b18adab3238e4c4
Charlie Curtis <charlie-curtis@outlook.com>
2022-09-28 11:23:22 -0400

    do another problem

--- untitled/src/CodeChef.java
+++ untitled/src/CodeChef.java
@@ -19,12 +19,40 @@ class Codechef
 
     int[] answers = new int[testCases];
     for (int i = 0; i < testCases; i++) {
-      double finishedRank = Double.parseDouble(sc.nextLine());
-      answers[i] = (int) Math.ceil(finishedRank / 25);
+      int arrayLength = Integer.parseInt(sc.nextLine());
+      int[] input = new int[arrayLength];
+      String line = sc.nextLine();
+      String[] nums = line.split(" ");
+      for (int j = 0; j < arrayLength; j++) {
+        input[j] = Integer.parseInt(nums[j]);
+      }
+      answers[i] = computeParity(input);
     }
 
     for (int i = 0; i < answers.length; i++) {
       System.out.println(answers[i]);
     }
+  }
+
+  private static int computeParity(int[] input)
+  {
+    //even + even = even;
+    //even + odd = odd
+    //odd + odd = even;
+
+    //If we have an odd number, we can't ever get rid of it by breaking it down. This is because
+    //an odd number breaks down into odd = odd + even, so we didn't really get rid of anything
+    int countOfEvenNumbers = 0;
+
+    for (int i = 0; i < input.length; i++) {
+      if (input[i] % 2 == 0) {
+        countOfEvenNumbers ++;
+      }
+    }
+    if (countOfEvenNumbers == input.length) {
+      //if all numbers are even, we're good
+      return 0;
+    }
+    return countOfEvenNumbers;
   }
 }
